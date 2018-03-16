1b9a1cf721e365977dda351330d0a82aaa1fd978
João Paulo Canário <jopacanario@gmail.com>
2018-03-15 23:03:53 -0300

    Fix issue #10 (#15)
    * Add CNAME
    
    * Remove CNAME
    
    * Fix leaderboard behavior in scores of not happened wods
    
    * Fix PR #14
    
    * Revert "Fix leaderboard behavior in scores of not happened wods"
    
    This reverts commit 6f0b07b686dfab6f566562da557f5d22b686235e.
    
    * Fix ternary operator
    
    In suggested improvement on PR #14 this error is raised on `return` token:
    `Syntax error: Unexpected token`
    
    * Add PR #14 suggestion
    
    * Fix responsible leaderboard
    
    Add react-accessible-accordion component
    
    Update style.css
    
    Minor code improvements
    
    * Code improvement
    
    Add suggestions made on PR #15
    
    * Code improvement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import {Helmet} from "react-helmet";
 import autoBind from 'react-autobind';
 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
+} from 'react-accessible-accordion';
+
 import Spinner from './components/Spinner/';
 import logo from './assets/logo.png';
 
@@ -30,19 +37,19 @@ class App extends Component {
   }
 
   ordinalSuffix(i) {
-      const j = i % 10;
-      const k = i % 100;
-      let suffix = 'th';
+    const j = i % 10;
+    const k = i % 100;
+    let suffix = 'th';
 
-      if (j === 1 && k !== 11) {
-          suffix = "st";
-      } else if (j === 2 && k !== 12) {
-          suffix = "nd";
-      } else if (j === 3 && k !== 13) {
-          suffix = "rd";
-      }
+    if (j === 1 && k !== 11) {
+      suffix = "st";
+    } else if (j === 2 && k !== 12) {
+      suffix = "nd";
+    } else if (j === 3 && k !== 13) {
+      suffix = "rd";
+    }
 
-      return `${i}${suffix}`;
+    return `${i}${suffix}`;
   }
 
   toggleCategory(event) {
@@ -64,6 +71,144 @@ class App extends Component {
         )
       }),
     });
+  }
+
+  getLeaderboardHead() {
+    const w = parseInt(window.innerWidth, 10);
+    let elem = null;
+
+    if (w <= 768) {
+      elem = (
+        <tr>
+          <th scope="col">
+            <h4>Atletas</h4>
+          </th>
+        </tr>
+      );
+    } else if (w <= 1320) {
+      elem = (
+        <tr>
+          <th scope="col">Atletas</th>
+          <th scope="col">18.1</th>
+          <th scope="col">18.2</th>
+          <th scope="col">18.2a</th>
+          <th scope="col">18.3</th>
+          <th scope="col">18.4</th>
+          <th scope="col">18.5</th>
+        </tr>
+      );
+    } else {
+      elem = (
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Atletas</th>
+          <th scope="col">Pontos</th>
+          <th scope="col">18.1</th>
+          <th scope="col">18.2</th>
+          <th scope="col">18.2a</th>
+          <th scope="col">18.3</th>
+          <th scope="col">18.4</th>
+          <th scope="col">18.5</th>
+        </tr>
+      );
+    }
+
+    return elem;
+  }
+
+  getScoreAsTable(scores) {
+    return scores.map((score, index) => {
+      let elem = ( score.rank ?
+        <td key={index}>
+          <strong>{ this.ordinalSuffix(score.rank) }</strong>
+          <br />
+          <small>({ score.scoreDisplay })</small>
+        </td>
+        : <td key={index} />
+      );
+
+      return elem;
+    });
+  }
+
+  getScoreAsList(scores) {
+    return scores.map((score, index) => {
+      const wods = [`18.1:`, `18.2:`, `18.2a:`, `18.3:`, `18.4:`, `18.5:`];
+
+      let elem = ( score.rank ?
+        <li key={wods[index]}>
+          <strong> {wods[index]}</strong> {this.ordinalSuffix(score.rank) } ({score.scoreDisplay})
+        </li>
+        : ''
+      );
+
+      return elem;
+    });
+  }
+
+  getLeaderboardContent(athletes) {
+    const w = parseInt(window.innerWidth, 10);
+    let elem = null;
+
+    if (w <=   768) {
+      elem = athletes.map((athlete, index) => {
+        return (
+          <tr key={index}>
+            <td>
+              <Accordion accordion={false}>
+                <AccordionItem>
+                  <AccordionItemTitle>
+                    <h6 className="u-position-relative">
+                      <strong>{athlete.overallRank} ({athlete.overallScore}) {athlete.competitorName}</strong>
+                      <br /> &nbsp;&nbsp;&nbsp;&nbsp;<small>{athlete.affiliateName}</small>
+                      <div className="accordion__arrow" role="presentation" />
+                    </h6>
+                  </AccordionItemTitle>
+                  <AccordionItemBody>
+                    <ul>
+                        { this.getScoreAsList(athlete.scores) }
+                    </ul>
+                  </AccordionItemBody>
+                </AccordionItem>
+              </Accordion>
+            </td>
+          </tr>
+        )
+      });
+    } else if (w <= 1320) {
+      elem = athletes.map((athlete, index) => {
+        return (
+          <tr key={index}>
+            <td>
+              <h6>
+                <strong>{athlete.overallRank} ({athlete.overallScore}) {athlete.competitorName}</strong>
+                <br /> &nbsp;&nbsp;<small>{athlete.affiliateName}</small>
+              </h6>
+            </td>
+            { this.getScoreAsTable(athlete.scores) }
+          </tr>
+        )
+      });
+    } else {
+      elem = athletes.map((athlete, index) => {
+        return (
+          <tr key={index}>
+            <th scope="row">{ athlete.overallRank }</th>
+            <td>
+              <strong>{ athlete.competitorName }</strong>
+              <br />
+              <small>{ athlete.affiliateName }</small>
+            </td>
+            <td>
+              <strong>{ athlete.overallScore }</strong>
+            </td>
+            { this.getScoreAsTable(athlete.scores) }
+          </tr>
+        )
+      });
+    }
+
+    return ( elem );
   }
 
   render() {
@@ -98,8 +243,7 @@ class App extends Component {
               <div className="col-md-auto col-lg-4">
 
                 <div className="btn-group">
-                  <button
-                    className={`btn btn-primary ${ category === 'isMen' ? 'active' : '' }`}
+                  <button className={`btn btn-primary ${ category === 'isMen' ? 'active' : '' }`}
                     dataref="isMen"
                     onClick={this.toggleCategory}
                   >
@@ -132,49 +276,11 @@ class App extends Component {
           <div className="table-responsive">
             <table id="leaderboardTable" className="table table-striped">
               <thead className="thead-dark">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Nome</th>
-                  <th scope="col">Pontos</th>
-                  <th scope="col">18.1</th>
-                  <th scope="col">18.2</th>
-                  <th scope="col">18.2a</th>
-                  <th scope="col">18.3</th>
-                  <th scope="col">18.4</th>
-                  <th scope="col">18.5</th>
-                </tr>
+                { this.getLeaderboardHead() }
               </thead>
 
               <tbody id="leaderboardBody">
-                {
-                  athletesList.map((athlete, index) => {
-                    return (
-                      <tr>
-                        <th scope="row">{ athlete.overallRank }</th>
-                        <td>
-                          <strong>{ athlete.competitorName }</strong>
-                          <br />
-                          <small>{ athlete.affiliateName }</small>
-                        </td>
-                        <td><strong>{ athlete.overallScore }</strong></td>
-
-                        {
-                          athlete.scores.map((score) => {
-                            return (
-                              score.rank ?
-                                <td>
-                                  <strong>{ this.ordinalSuffix(score.rank) }</strong>
-                                  <br />
-                                  <small>({ score.scoreDisplay })</small>
-                                </td>
-                              : <td />
-                            )
-                          })
-                        }
-                      </tr>
-                    )
-                  })
-                }
+                { this.getLeaderboardContent(athletesList) }
               </tbody>
 
             </table>
